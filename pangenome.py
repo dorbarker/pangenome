4227c669963efb9b83334d31692c8970d17a0dad
Dillon Barker <dor.barker@gmail.com>
2016-03-27 13:22:35 -0600

    Add formatter for settings.txt

--- pangenome.py
+++ pangenome.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 
 import argparse
-from getpass import getuser
+import csv
+from multiprocessing import cpu_count
 import os
 import subprocess
 import sys
@@ -15,6 +16,12 @@ def arguments():
 
     parser.add_argument('--work-dir', help = 'Path to work directory \
                                       (default = one level up from fasta-dir)')
+
+    parser.add_argument('--word_size', type = int, default = 20,
+                        help = 'Word size for BLASTN (default = 20)')
+
+    parser.add_argument('--cores', type = int, default = cpu_count(),
+                        help = 'Number of CPU cores to use')
 
     return parser.parse_args()
 
@@ -35,20 +42,50 @@ def find_panseq():
         return out
 
 def find_requirements():
+    '''Locates blastn, mummer, and muscle'''
 
     def which(name, parent_dir = True):
-        
+ 
         try:
             result = subprocess.check_output(['which', name]).strip()
 
             return os.path.dirname(result) + '/' if parent_dir else result
-        
+
         except subprocess.CalledProcessError:
 
             sys.stderr.write('FATAL: Cannot locate {} in PATH\n'.format(name))
             sys.exit(1)
 
     _args = (('blastn',), ('mummer',), ('muscle', False))
-    
+
     return {args[0]: which(*args) for args in _args}
+
+def format_settings(fasta_dir, work_dir, reqs, size, perc_id, word, cores):
+    '''Perpares the settings file panseq takes as its only argument'''
+
+    num_strains = sum(1 for x in os.listdir(fasta_dir) if '.f' in x)
+    base_dir = os.path.join(work_dir, 'panseq_output/')
+
+    settings = (
+        ('queryDirectory',         fasta_dir),
+        ('baseDirectory',          base_dir),
+        ('numberOfCores',          cores),
+        ('mummerDirectory',        reqs['mummer']),
+        ('blastDirectory',         reqs['blastn']),
+        ('muscleExecutable',       reqs['muscle']),
+        ('novelRegionFinderMode',  'no_duplicates'),
+        ('minimumNovelRegionSize', size),
+        ('fragmentationSize',      size),
+        ('percentIdentityCutoff',  perc_id),
+        ('coreGenomeThreshold',    num_strains)
+        ('runMode',                'pan'),
+        ('blastWordSize',          word),
+        ('frameshift',             1),
+        ('overwrite',              1)
+    )
+
+    with open(os.path.join(work_dir, 'settings.txt'), 'w') as f:
+        out = csv.writer(delimiter = '\t')
+        for line in settings:
+            out.writerow(line)
 
